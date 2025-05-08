a6b524844abc1b794450391647cf5fe90f79dbd1
gauravbagal66 <gauravbagal66@gmail.com>
2025-05-08 20:29:47 +0530

    New updated code

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f374f9756305322"; }
+/******/ 		__webpack_require__.h = function() { return "55aca724b0c9dd33"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/style.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
